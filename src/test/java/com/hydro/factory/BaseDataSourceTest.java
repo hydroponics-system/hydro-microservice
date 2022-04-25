e1c9974b0bcaf493d67803ae1cad6fab27af615c
Samuel Butler <sambutler1017@icloud.com>
2022-04-25 03:18:11 -0400

    dont drop

--- src/test/java/com/hydro/factory/BaseDataSourceTest.java
+++ src/test/java/com/hydro/factory/BaseDataSourceTest.java
@@ -42,7 +42,7 @@ public abstract class BaseDataSourceTest {
     @AfterAll
     public static void cleanUp() {
         LOGGER.info("Dropping test schema...");
-        dropSchema();
+        // dropSchema();
 
     }
 
