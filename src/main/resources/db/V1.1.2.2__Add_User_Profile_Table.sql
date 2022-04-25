4afde254cb2d81b722feada1e69022eafbb85d9a
Samuel Butler <sambutler1017@icloud.com>
2022-04-25 03:31:43 -0400

    maybe

--- src/main/resources/db/V1.1.2.2__Add_User_Profile_Table.sql
+++ src/main/resources/db/V1.1.2.2__Add_User_Profile_Table.sql
@@ -18,10 +18,10 @@ CREATE TABLE user_profile (
   web_role_id              int(10)      unsigned NOT NULL DEFAULT 1,
   last_login_date_utc      datetime              NOT NULL DEFAULT current_timestamp(),
   insert_date_utc          datetime              NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (id),
-  UNIQUE KEY user_profile_AK1 (email),
-  KEY user_profile_IDX1 (web_role_id)
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE UNIQUE INDEX user_profile_AK1 ON user_profile(email);
 
 ALTER TABLE user_profile ADD CONSTRAINT web_role__user_profile__FK1 
 FOREIGN KEY (web_role_id) REFERENCES web_role (id) ON UPDATE CASCADE;
