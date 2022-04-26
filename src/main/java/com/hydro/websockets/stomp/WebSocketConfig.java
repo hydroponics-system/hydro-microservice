bdfc3c27a0f243aa5455b810db8bbd71f582dfd8
Samuel Butler <sambutler1017@icloud.com>
2022-04-25 22:17:20 -0400

    Removing user handshake handler

--- src/main/java/com/hydro/websockets/stomp/WebSocketConfig.java
+++ src/main/java/com/hydro/websockets/stomp/WebSocketConfig.java
@@ -37,6 +37,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/api/websocket").setHandshakeHandler(new UserHandshakeHandler()).setAllowedOrigins("*");
+        registry.addEndpoint("/api/websocket").setAllowedOrigins("*");
     }
 }
