4417516c7401b36d58e727be4f6c567c61694026
Samuel Butler <sambutler1017@icloud.com>
2022-05-29 22:17:34 -0400

    DEVELOPMENT: Adding test for has access and utilizing @ParameterizedTest

--- src/test/java/com/hydro/common/util/CommonUtilTest.java
+++ src/test/java/com/hydro/common/util/CommonUtilTest.java
@@ -8,6 +8,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import com.hydro.factory.annotations.HydroTest;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @HydroTest
 public class CommonUtilTest {
@@ -19,17 +21,19 @@ public class CommonUtilTest {
         assertTrue(value.toString().length() == 10, "Length of random number should be 10");
     }
 
-    @Test
-    public void testGenerateRandomNumberWithLength() {
-        Long value = CommonUtil.generateRandomNumber(6);
+    @ParameterizedTest
+    @ValueSource(ints = { 1, 2, 3, 4, 5, 6, 7, 8, 9 })
+    public void testGenerateRandomNumberWithLength(int number) {
+        Long value = CommonUtil.generateRandomNumber(number);
         assertNotNull(value);
-        assertTrue(value.toString().length() == 6, "Length of random number should be 6");
+        assertTrue(value.toString().length() == number, "Length of random number should be " + number);
     }
 
-    @Test
-    public void testGenerateRandomNumberWithInvalidLength() {
+    @ParameterizedTest
+    @ValueSource(ints = { 100, 0, 15, -1 })
+    public void testGenerateRandomNumberWithInvalidLength(int number) {
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
-                () -> CommonUtil.generateRandomNumber(100));
+                () -> CommonUtil.generateRandomNumber(number));
         assertEquals("Length must be between 0 and 10", e.getMessage(), "Exception Message");
     }
 }
