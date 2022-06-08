da04592fb89c5ae5083e940f2290ab42bb43cde3
Samuel Butler <sambutler1017@icloud.com>
2022-06-08 01:04:55 -0400

    DEVELOPMENT: Running test when build runs

--- src/test/resources/scripts/system/hydrosystemDAO/init.sql
+++ src/test/resources/scripts/system/hydrosystemDAO/init.sql
@@ -1,4 +1,11 @@
 DELETE FROM systems;
+DELETE FROM user_profile;
+
+INSERT INTO user_profile (id, first_name, last_name, email, web_role)
+VALUES 
+(1, 'Test', 'User', 'test@mail.com', 'USER'),
+(2, 'Bill', 'Tanner', 'billT@mail.com', 'USER'),
+(3, 'Fake', 'User', 'Fake123@mail.com', 'DEVELOPER');
 
 INSERT INTO systems(id, uuid, part_number, name, insert_user_id)
 VALUES
